e817cc72e5c88b153a75a17613df09b1aec92157
BParr03 <Benparr03@gmail.com>
2025-01-24 16:21:42 +0000

    feat: Responsive Designs for about page

--- contact.js
+++ contact.js
@@ -1,20 +1,20 @@
-document.querySelector('.github-link').addEventListener('mouseover', () => {
+// document.querySelector('.github-link').addEventListener('mouseover', () => {
     
-  });
+//   });
   
-document.querySelector('.github-link').addEventListener('mouseleave', () => {
-  });
+// document.querySelector('.github-link').addEventListener('mouseleave', () => {
+//   });
 
-document.querySelector('.linkedin-link').addEventListener('mouseover', () => {
+// document.querySelector('.linkedin-link').addEventListener('mouseover', () => {
     
-  });
+//   });
   
-document.querySelector('.linkedin-link').addEventListener('mouseleave', () => {
-  });
+// document.querySelector('.linkedin-link').addEventListener('mouseleave', () => {
+//   });
 
-document.querySelector('.email-link').addEventListener('mouseover', () => {
+// document.querySelector('.email-link').addEventListener('mouseover', () => {
     
-  });
+//   });
   
-document.querySelector('.email-link').addEventListener('mouseleave', () => {
-  });
+// document.querySelector('.email-link').addEventListener('mouseleave', () => {
+//   });
